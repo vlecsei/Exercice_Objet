1790db175af11215571ba6c4a1efc5356ba62a29
vlecsei <vlecsei@gmail.com>
2018-08-23 16:38:52 +0200

    RAZ

---
+++ src/com/vladimir/vivant/Humains.java
@@ -0,0 +1,5 @@
+package com.vladimir.vivant;
+
+public class Humains {
+
+}
---
+++ src/com/vladimir/vivant/Vivants.java
@@ -0,0 +1,10 @@
+package com.vladimir.vivant;
+
+public class Vivants {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
