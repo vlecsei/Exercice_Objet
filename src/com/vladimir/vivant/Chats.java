72f3d8884997034f76f18b4ab8b93bea96cac5bf
vlecsei <vlecsei@gmail.com>
2018-08-24 09:57:48 +0200

    Initialisation

--- src/com/vladimir/vivant/Chats.java
+++ src/com/vladimir/vivant/Chats.java
@@ -1,5 +1,35 @@
 package com.vladimir.vivant;
 
-public class Chats {
+public class Chats extends Vivants{
+    
+    
+    private int nbrePattes;
+
+    public int getNbrePattes() {
+        return nbrePattes;
+    }
+
+    public void setNbrePattes(int nbrePattes) {
+        this.nbrePattes = nbrePattes;
+    }
+
+    public Chats(String nourriture, String boisson, String communication, String surnom, int nbrePattes) {
+	super(nourriture, boisson, communication, surnom);
+	this.nbrePattes = nbrePattes;
+    }
+
+    @Override
+    public String toString() {
+	return "Chats [nbrePattes=" + nbrePattes + ", nourriture=" + nourriture + ", boisson=" + boisson
+		+ ", communication=" + communication + ", surnom=" + surnom + "]";
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
