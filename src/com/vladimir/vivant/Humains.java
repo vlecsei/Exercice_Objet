40e37533a30bbf43e9706ef88d9784f1d34575ca
vlecsei <vlecsei@gmail.com>
2018-08-24 10:02:02 +0200

    Init

--- src/com/vladimir/vivant/Humains.java
+++ src/com/vladimir/vivant/Humains.java
@@ -1,5 +1,48 @@
 package com.vladimir.vivant;
 
-public class Humains {
-
+public class Humains extends Vivants {
+    
+    
+    private String nom;
+    private int nbrePieds;
+    private boolean emploi;
+    
+    
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public int getNbrePieds() {
+        return nbrePieds;
+    }
+    public void setNbrePieds(int nbrePieds) {
+        this.nbrePieds = nbrePieds;
+    }
+    public boolean isEmploi() {
+        return emploi;
+    }
+    public void setEmploi(boolean emploi) {
+        this.emploi = emploi;
+    }
+    public Humains(String nourriture, String boisson, String communication, String surnom, String nom, int nbrePieds,
+	    boolean emploi) {
+	super(nourriture, boisson, communication, surnom);
+	this.nom = nom;
+	this.nbrePieds = nbrePieds;
+	this.emploi = emploi;
+    }
+    @Override
+    public String toString() {
+	return "Humains [nom=" + nom + ", nbrePieds=" + nbrePieds + ", emploi=" + emploi + ", nourriture=" + nourriture
+		+ ", boisson=" + boisson + ", communication=" + communication + ", surnom=" + surnom + "]";
+    }
+    
+    
+    
+    
+    
+    
+    
 }
--- src/com/vladimir/vivant/Vivants.java
+++ src/com/vladimir/vivant/Vivants.java
@@ -1,10 +1,71 @@
 package com.vladimir.vivant;
 
 public class Vivants {
+    
+ 
+    static String nourriture;
+    static String boisson;
+    static String communication;
+    static String surnom;
+    
+    
+    
+    
+  // GETTERS ET SETTERS---------------------------------------------------------------------------------------------------
+    //NOURRITURE------------------------------
+    public static String getNourriture() {
+        return nourriture;
+    }
+    public static void setNourriture(String nourriture) {
+        Vivants.nourriture = nourriture;
+    }
+
+    //BOISSON-----------------------------------
+    public static String getBoisson() {
+        return boisson;
+    }
+    public static void setBoisson(String boisson) {
+        Vivants.boisson = boisson;
+    }
+
+    //COMMUNICATION---------------------------------
+    public static String getCommunication() {
+        return communication;
+    }
+    public static void setCommunication(String communication) {
+        Vivants.communication = communication;
+    }
+
+    //SURNOM-----------------------------------------
+    public static String getSurnom() {
+        return surnom;
+    }
+    public static void setSurnom(String surnom) {
+        Vivants.surnom = surnom;
+    }
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+//CONSTRUCTEUR----------------------------------------------------------------------------------------------------------
+   public Vivants(String nourriture, String boisson, String communication, String surnom) {
+    super();
+    Vivants.nourriture = nourriture;
+    Vivants.boisson = boisson;
+    Vivants.communication = communication;
+    Vivants.surnom = surnom;
+}
+
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	
+	
+	
+	
+	
+	
+	
 
     }
+
 
 }
